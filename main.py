090b053b92b948631ed93994fb30d6269435ded7
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2023-01-14 19:54:55 -0400

    xml

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from xml.dom import minidom
 from abc import ABC, abstractmethod
 
 
@@ -21,17 +22,17 @@ class Padre:
             print(AveMessure)
             return AveMessure
 
-    def Calc(self,calc,AveMessure):
-        AveMessure = []
+#     def Calc(self,calc,AveMessure):
+#         AveMessure = []
 
-        n = len(AveMessure)
-        total= (n(calc))/calc.len()
-        MaxValue = (max(calc))
-        MinValue =(min(calc))
-        print(total,MaxValue,MinValue)
+#         n = len(AveMessure)
+#         total= (n(calc))/calc.len()
+#         MaxValue = (max(calc))
+#         MinValue =(min(calc))
+#         print(total,MaxValue,MinValue)
 
-p1=Padre()
-p1.run()
+# p1=Padre()
+# p1.run()
 #p1.Calc() 
 #captura de datos del json
 
@@ -54,5 +55,18 @@ p1.run()
 
 # with open ('dataout.json','w') as outfile:
 #     json.dump(jdata, outfile)
+class xml(Padre):
+    def readxml(self):
+        mydoc = minidom.parse('data.xml')
+        items = mydoc.getElementsByTagName('meassure')
 
+        print('\All attributes:')
+    print(doc.nodeName)
+
+    
+    def run(self):
+         self.readxml()
+x1=xml()
+x1.re()
+    
 
